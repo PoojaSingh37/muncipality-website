7ad39864dc8f860cec154311c0a64030dffaf092
Pooja Singh <90820313+PoojaSingh37@users.noreply.github.com>
2024-11-24 23:01:52 +0545

    adjust route

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -1,20 +1,20 @@
-import Dashboard from '@Views/Dashboard';
+// import Dashboard from '@Views/Dashboard';
 import LandingPage from '@Views/Landingpage';
 import { IRoute } from './types';
 
 const appRoutes: IRoute[] = [
   {
     path: '/',
-    name: 'Dashboard ',
-    component: Dashboard,
-    authenticated: false,
-  },
-  {
-    path: '/Landingpage',
-    name: 'Landingpage ',
+    name: 'landingpage',
     component: LandingPage,
     authenticated: false,
   },
+  // {
+  //   path: '/Landingpage',
+  //   name: 'Landingpage ',
+  //   component: LandingPage,
+  //   authenticated: false,
+  // },
 ];
 
 export default appRoutes;
